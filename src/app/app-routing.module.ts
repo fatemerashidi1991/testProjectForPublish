2bbe114ad516d6dd810b6e065157992d3664906c
fateme.rashidi1991 <rashidi.ftm@gmail.com>
2019-05-19 12:46:38 +0430

    make register page as defualt route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { RegisterComponent } from '../app/register/register.component';
 import { AuthGuard } from '../app/guards/auth.guard';
 
 const appRoutes: Routes = [
-     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
      { path: 'login', component: LoginComponent },
      { path: 'register', component: RegisterComponent },
 
     // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'register' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
